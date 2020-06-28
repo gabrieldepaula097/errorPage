c834249d00e3f8a86a8bb33a601e340064258f8b
Gabriel de Paula <gabrieldepaula@localhost.localdomain>
2020-06-28 20:51:50 -0300

    faltando apenas ajuste mobile e tablet portrait

--- pages/ErrorMessage.vue
+++ pages/ErrorMessage.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="text-center text-white align-middle max-w-100">
-    <p class="font-thin text-xl text-textColorH1">Oops...</p>
-    <h1 class="font-bold text-xl">404</h1>
-    <button class="py-4 px-6"> Página Não Encontrada</button>
-    <div class="p-2 justify-between flex flex-row">
-       <div class="w-1/2 h-12 m-auto">
-        <a href="" class="text-sm">
-          <font-awesome-icon :icon="['fas', 'arrow-left']"/> Voltar
-        </a>
-       </div>
-       <div class="w-1/2 h-12 m-auto">
-        <a href="">
-          <font-awesome-icon :icon="['fas', 'home']"/> Home
-        </a>
-       </div>
+  <div class="text-white flex items-center justify-center max-w-100 error">
+    <div class="max-w-50">
+      <p class="font-thin error-text-top text-textColorH1 text-right">Oops...</p>
+      <h1 class="font-bold error-text-center">404</h1>
+      <p class="error-text-bottom text-center"> Página Não Encontrada</p>
+      <div class="justify-between flex items-center max-w-98">
+        <div class="h-12">
+          <a href="#" class="text-sm">
+            <font-awesome-icon :icon="['fas', 'arrow-left']"/> Voltar
+          </a>
+        </div>
+        <div class="h-12">
+          <a href="#">
+            <font-awesome-icon :icon="['fas', 'home']"/> Home
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- pages/Footer.vue
+++ pages/Footer.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="container text-center text-white align-middle justify-between">
-    <div class="flex flex-row mb-4 ml-4 align-middle justify-between">
+  <footer class="container text-center text-white align-middle justify-between max-h-1/4">
+    <div class="flex flex-row mb-2 ml-4 align-middle justify-between">
       <div class="w-1/2 h-auto text-left">
-        <img src="@/assets/logo-branco.svg" width="150" class="p-2 inline-block text-left">
+        <img src="@/assets/logo-branco.svg" class="p-2 inline-block text-left">
         <br>
         <span class="text-center mb-4 ml-4">made with ❤ in Brazil</span>
         <div class="align-middle flex mb-4 px-2">
-          <img src="@/assets/baixarios.svg" width="175" class="bg-transparent pr-2">
-          <img src="@/assets/baixargoogleplay.svg" width="175" class="bg-transparent">
+          <img src="@/assets/baixarios.svg" class="bg-transparent pr-2 max-w-15">
+          <img src="@/assets/baixargoogleplay.svg" class="bg-transparent max-w-15">
         </div>
       </div>
       <div class="w-1/2 h-auto text-right">
         <div class="flex flex-row mb-4 ml-4 justify-end">
-          <div class="w-1/4 h-12 float-right">
+          <div class="h-12 float-right w-1/4">
             <div class="d-none text-left d-lg-flex flex-column col">
-              <h5 class="mb-2 textColorH1">Influu</h5>
+              <h5 class="mb-1 textColorH1">Influu</h5>
               <ul>
                 <li><a href="/pt/about-us" class="p-0 normal-case">About</a></li>
                 <li><a href="/pt/terms-of-use" class="p-0 normal-case">Terms of use</a></li>
@@ -23,9 +23,9 @@
               </ul>
             </div>
           </div>
-          <div class="w-1/4 h-12 float-right">
+          <div class="h-12 w-1/4 float-right footer-col">
             <div class="d-none text-left d-lg-flex flex-column col">
-              <h5 class="mb-2 textColorH1">Product</h5>
+              <h5 class="mb-1 textColorH1">Product</h5>
               <ul>
                 <li><a href="/pt#content3" class="p-0 normal-case">How does it work?</a></li>
                 <li><a href="/pt#content4" class="p-0 normal-case">Plataform</a></li>
@@ -36,7 +36,7 @@
           </div>
           <div class="w-1/4 h-12 float-right">
             <div class="d-none text-left d-lg-flex flex-column col">
-              <h5 class="mb-2 textColorH1">Talk to us</h5>
+              <h5 class="mb-1 textColorH1">Talk to us</h5>
               <ul>
                 <li><a href="/pt#content5" class="p-0 normal-case">Contact</a></li>
                 <li><a href="" class="p-0 normal-case">Press </a></li>
@@ -48,24 +48,37 @@
       </div>
     </div>
     <hr>
-    <div class="flex flex-row mb-4 ml-4 mt-4">
-      <p class="w-1/2 h-12 mb-4 text-left">
+    <div class="flex flex-row ml-4 mt-4">
+      <p class="w-1/2 h-12 mb-2 text-left text-xs">
         Teodoro Sampaio Street, 2767 Office 22 - Pinheiros, São Paulo, Brasil - CEP: 05405-250
         <br>
         oi@influu.me
         <br>
         CNPJ: 19.354.277/0001-40
       </p>
-      <div class="w-1/2 h-12 mb-4 text-right pl-0 pb-0 pt-0">
+      <div class="w-1/2 h-12 mb-2 text-right pl-0 pb-0 pt-0 text-xs">
         Copyright©<a class="font-bold text-right normal-case">influu.me 2020</a>
         <br>
-        <a class="pl-2 pr-2 pb-0 pt-0 d-inline-block">All Rights Reserved</a>
+        <a class="pl-2 pr-2 pb-0 pt-0 d-inline-block text-xs -mt-5">All Rights Reserved</a>
       </div>
     </div>
-  </div>
+  </footer>
 </template>
 
 <script>
 export default {
+  // data () {
+  //   return {
+  //     windowVar: window
+  //   }
+  // },
+  // methods: {
+  //   getWidth () {
+  //     return this.windowVar.innerWidth
+  //   },
+  //   getHeight () {
+  //     return this.windowVar.innerHeight
+  //   }
+  // }
 }
 </script>
